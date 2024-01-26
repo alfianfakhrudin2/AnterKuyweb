f7d775a992a4aa661161550b86c4462bde0e1666
Alfian <alfianfakhrudin11@gmail.com>
2024-01-26 17:01:36 +0700

    navbar udah jadi

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -1,12 +1,12 @@
-var icon = document.getElementById("icon");
-icon.onclick = function () {
-    document.body.classList.toggle("modegelap");
-    if (document.body.classList.contains("modegelap")) {
-        icon.src = "img/sun.png";
-    } else {
-        icon.src = "img/moon.png";
-    }
-}
+// var icon = document.getElementById("icon");
+// icon.onclick = function () {
+//     document.body.classList.toggle("modegelap");
+//     if (document.body.classList.contains("modegelap")) {
+//         icon.src = "img/sun.png";
+//     } else {
+//         icon.src = "img/moon.png";
+//     }
+// }
 
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.querySelector(".toggle-button");
@@ -16,3 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
         navList.classList.toggle("show");
     });
 });
+
+// const menu = document.getElementById("menu");
+// const navbarMenu = document.querySelector(".navbar-menu");
+
+// menu.onclick = function () {
+//     if (navbarMenu.style.display == "flex") {
+//         navbarMenu.style.display = "none";
+//     } else {
+//         navbarMenu.style.display = "flex";
+//     }
+// };
