dbdbb53f25ed5e3c7489e8c413d67a12b4e4a7c2
Alfian <alfianfakhrudin11@gmail.com>
2024-02-04 20:44:35 +0700

    merapihkan code

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -1,13 +1,3 @@
-// var icon = document.getElementById("icon");
-// icon.onclick = function () {
-//     document.body.classList.toggle("modegelap");
-//     if (document.body.classList.contains("modegelap")) {
-//         icon.src = "img/sun.png";
-//     } else {
-//         icon.src = "img/moon.png";
-//     }
-// }
-
 document.addEventListener("DOMContentLoaded", function (x) {
     const toggleButton = document.querySelector(".toggle-button");
     const navList = document.querySelector(".nav-list");
@@ -49,18 +39,6 @@ icon.onclick = function () {
     }
 }
 
-// function menuDisplay(state, items) {
-//     if (state == "show") {
-//         for (var i = 1; i < items.length; i++) {
-//             items[i].classList.add("show");
-//         }
-//     } else {
-//         for (var i = 1; i < items.length; i++) {
-//             items[i].classList.remove("show");
-//         }
-//     }
-// }
-
 window.onload = function () {
     main();
 }
@@ -69,15 +47,3 @@ window.onload = function () {
 const yearSpan = document.querySelector("#currentYear");
 const currentYear = new Date();
 yearSpan.innerText = currentYear.getFullYear();
-
-
-// const menu = document.getElementById("menu");
-// const navbarMenu = document.querySelector(".navbar-menu");
-
-// menu.onclick = function () {
-//     if (navbarMenu.style.display == "flex") {
-//         navbarMenu.style.display = "none";
-//     } else {
-//         navbarMenu.style.display = "flex";
-//     }
-// };
